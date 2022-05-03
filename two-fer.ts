978046cb483618f0d583fd19e306d4e9a0a7a7a1
efraintinoco <oakcliff_21@yahoo.com>
2022-05-03 15:59:58 -0500

    added test

--- two-fer.ts
+++ two-fer.ts
@@ -1,1 +1,8 @@
-export function twoFer() {}
+export function twoFer(name: string) {
+    if (name == null) {
+        return `One for you, one for me.`
+    } else {
+        return `One for ${name}, one for me.`
+
+    }
+}
